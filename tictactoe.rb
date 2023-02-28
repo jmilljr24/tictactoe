d6b15eed977f19b8b54bd8530ce4aa8d25492910
jmilljr24 <jmilljr24@gmail.com>
2023-02-28 13:22:35 -0500

    Add initial game play with first player input

--- tictactoe.rb
+++ tictactoe.rb
@@ -6,6 +6,8 @@ class User
     end
 end
 
+
+
 class Board
     attr_reader :display_board
     def initialize
@@ -29,3 +31,37 @@ class Board
     end
 end
 
+class Game
+    attr_reader :board, :player1, :player2, :current_player
+    def initialize
+        @board = Board.new
+        @player1 = User.new("Player One", "X")
+        @player2 = User.new("Player Two", "O")
+        p @current_player = player1
+    end
+
+    def display
+        board.display_board
+    end
+
+    def next_turn
+        postion = gets.chomp
+    end
+end
+
+def begin_game
+    puts "Player One you are X"
+    puts "Player Two you are O"
+    puts "Lets Begin - Player one, choose a position 1-9"
+    #symbol = @player1.symbol
+    #board.update_board(position, symbol)
+end
+
+def play_game
+    begin_game
+    game = Game.new
+    game.display
+    game.next_turn
+end
+
+play_game
