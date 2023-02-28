b004e7887e596861baf991df019854ad3034b1a9
jmilljr24 <jmilljr24@gmail.com>
2023-02-28 14:17:13 -0500

    Add method to switch player in game play

--- tictactoe.rb
+++ tictactoe.rb
@@ -9,7 +9,7 @@ end
 
 
 class Board
-#   attr_reader :display_board
+   attr_reader :cells
     def initialize
         @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     end
@@ -17,18 +17,21 @@ class Board
     def display_board
         puts <<-HEREDOC
 
-          #{@cells[0]} | #{@cells[1]} | #{@cells[2]}
+          #{cells[0]} | #{cells[1]} | #{cells[2]}
          ---+---+---
-          #{@cells[3]} | #{@cells[4]} | #{@cells[5]}
+          #{cells[3]} | #{cells[4]} | #{cells[5]}
          ---+---+---
-          #{@cells[6]} | #{@cells[7]} | #{@cells[8]}
+          #{cells[6]} | #{cells[7]} | #{cells[8]}
         
         HEREDOC
     end
 
     def update_board(number, symbol)
-        @cells[number] = symbol
+        cells[number - 1] = symbol
     end
+    def full?
+        cells.all? { |cell| cell =~ /[^0-9]/ }
+      end
 end
 
 class Game
@@ -37,17 +40,35 @@ class Game
         @board = Board.new
         @player1 = User.new("Player One", "X")
         @player2 = User.new("Player Two", "O")
-        @current_player = player1
+        @current_player = nil
     end
 
     def display
         board.display_board
     end
 
-    def next_turn
+    def player_turns
+        @current_player = player1
+        until board.full?
+            next_turn(current_player)
+           # break if board.game_over?
+           @current_player = switch_current_player
+        end
+    end
+
+    def next_turn(player)
         position = gets.chomp
         board.update_board(position.to_i, current_player.symbol)
         self.display
+    end
+    def switch_current_player
+        if current_player == player1
+          player2
+        else
+          player1
+        end
+      end
+    def game_over?
     end
 end
 
@@ -55,15 +76,13 @@ def begin_game
     puts "Player One you are X"
     puts "Player Two you are O"
     puts "Lets Begin - Player one, choose a position 1-9"
-    #symbol = @player1.symbol
-    #board.update_board(position, symbol)
 end
 
 def play_game
     begin_game
     game = Game.new
     game.display
-    game.next_turn
+    game.player_turns
 end
 
 play_game
